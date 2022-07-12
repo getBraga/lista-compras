ddfa432a02ac2c0fbdad1f3b1156f321d868aad3
Ysmael Braga <ysmaelbraga.prestserv@petrobras.com.br>
2022-07-11 22:06:53 -0300

    configurado o sass com o buefy

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,6 +2,7 @@
   <div>
     <section>
       <div>
+        <b-button  type="is-primary">aaaaaaaa</b-button>
         <Nuxt />
       </div>
     </section>
@@ -19,6 +20,63 @@ export default {
 <style lang="scss">
 @import "~/assets/sass/app.scss";
 @import "~bulma/sass/utilities/_all";
+
+.button.is-primary {
+  background: red;
+}
+
+$primary: $cor-principal;
+$primary-invert: findColorInvert($primary);
+$twitter: #4099ff;
+$twitter-invert: findColorInvert($twitter);
+$colors: (
+  "white": (
+    $white,
+    $black,
+  ),
+  "black": (
+    $black,
+    $white,
+  ),
+  "light": (
+    $light,
+    $light-invert,
+  ),
+  "dark": (
+    $dark,
+    $dark-invert,
+  ),
+  "primary": (
+    $primary,
+    $primary-invert,
+  ),
+  "info": (
+    $info,
+    $info-invert,
+  ),
+  "success": (
+    $success,
+    $success-invert,
+  ),
+  "warning": (
+    $warning,
+    $warning-invert,
+  ),
+  "danger": (
+    $danger,
+    $danger-invert,
+  ),
+  "twitter": (
+    $twitter,
+    $twitter-invert,
+  ),
+);
+$link: $primary;
+$link-invert: $primary-invert;
+$link-focus-border: $primary;
+@import "~bulma";
+@import "~buefy/src/scss/buefy";
+@import "~/assets/sass/app.scss";
 
 
 </style>
