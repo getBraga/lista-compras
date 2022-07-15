347645a0099fd8c4379f6dcaeed3068f12e8b37e
Ysmael Braga <ysmaelbraga01@gmail.com>
2022-07-15 01:08:12 -0300

    feito o componente do footer

---
+++ components/Footer/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <footer><span>Todos os direitos reservados.</span></footer>
+</template>
+
+<script>
+export default {
+  name: 'FooterComponent',
+}
+</script>
+
+<style scoped>
+footer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 50px;
+  margin: 0px;
+  color: white;
+  width: 100%;
+  background: #ac6c47;
+}
+</style>
