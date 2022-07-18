805e6c11f1f22e919b79cce45c054d3e216a0348
Ysmael Braga <ysmaelbraga01@gmail.com>
2022-07-18 00:58:40 -0300

    iniciado o crud de update e delete

--- pages/lista-compras.vue
+++ pages/lista-compras.vue
@@ -8,18 +8,20 @@
         searchable
         sortable
         field="nome_produto"
-        label="Nome do Produto"
+        label="Nome"
       >
-        <b-input v-model="props.row.nome_produto" disabled />
+        <span v-if="!props.row.podeAlterar">{{ props.row.nome_produto }}</span>
+        <b-input v-else v-model="alterarNome" />
       </b-table-column>
 
       <b-table-column
         v-slot="props"
         sortable
         field="preco_produto"
-        label="Preço do Produto"
+        label="Preço"
       >
-        <b-input v-model="props.row.preco_produto" disabled />
+        <span v-if="!props.row.podeAlterar">{{ props.row.preco_produto }}</span>
+        <b-input v-else v-model="alterarPreco" />
       </b-table-column>
 
       <b-table-column
@@ -28,36 +30,64 @@
         label="Quantidade"
         sortable
       >
-        <b-input v-model="props.row.quantidade" disabled />
+        <span v-if="!props.row.podeAlterar">{{ props.row.quantidade }}</span>
+        <b-input v-else v-model="alterarQuantidade" />
       </b-table-column>
 
-      <b-table-column
-        v-slot="props"
-        field="preco_total"
-        label="Preço Total"
-        sortable
-      >
-        <b-input v-model="props.row.preco_total" />
+      <b-table-column v-slot="props" field="preco_total" label="Total" sortable>
+        <span>{{ props.row.preco_total }}</span>
       </b-table-column>
-      <b-table-column
-        v-slot="props"
-        field="excluir_produto"
-        label="Excluir"
-        centered
-      >
+      <b-table-column v-slot="props" field="excluir_produto" label="Excluir">
         <b-button class="btn-icon" @click="excluir(props.row)"
-          ><b-icon icon="delete" type="is-primary" size="is-medium"> </b-icon
+          ><b-icon class="icon-lixeira" icon="delete" size="is-medium"> </b-icon
         ></b-button>
       </b-table-column>
       <b-table-column
         v-slot="props"
         field="atualzar_produto"
-        label="Atualizar Produto"
+        label="Atualizar"
         centered
       >
-        <b-button class="btn-icon" @click="excluir(props.row)"
-          ><b-icon icon="pencil" type="is-primary" size="is-medium"> </b-icon
-        ></b-button>
+        <b-button
+          v-show="!props.row.podeAlterar"
+          class="btn-icon"
+          @click="ativarAlterar(props.row)"
+          ><b-icon
+            class="icon-primary"
+            icon="pencil"
+            type="is-primary"
+            size="is-medium"
+          >
+          </b-icon>
+        </b-button>
+        <div v-show="props.row.podeAlterar" class="icon-atualizar">
+          <b-button
+            v-show="props.row.podeAlterar"
+            class="btn-icon"
+            @click="props.row.podeAlterar = false"
+          >
+            <b-icon
+              class="close-icon"
+              icon="close-circle-outline"
+              type="is-second"
+              size="is-medium"
+            >
+            </b-icon
+          ></b-button>
+          <b-button
+            v-show="props.row.podeAlterar"
+            class="btn-icon"
+            @click="alterar(props.row)"
+          >
+            <b-icon
+              class="icon-primary-hover"
+              icon="check-circle"
+              type="is-primary"
+              size="is-medium"
+            >
+            </b-icon
+          ></b-button>
+        </div>
       </b-table-column>
       <template #empty>
         <div class="has-text-centered">
@@ -77,6 +107,9 @@ export default {
       sortOrder: 'desc',
       defaultSortOrder: 'desc',
       data: [],
+      alterarNome: null,
+      alterarPreco: null,
+      alterarQuantidade: null,
     }
   },
   mounted() {
@@ -84,10 +117,45 @@ export default {
   },
   methods: {
     excluir(data) {
-      // this.data = []
-      // eslint-disable-next-line no-console
-      console.log(data)
+      this.$buefy.dialog.confirm({
+        title: 'Deleting account',
+        message: `Tem certeza que deseja excluir o item ${data.nome_produto}?`,
+        confirmText: 'Deletar',
+        type: 'is-danger',
+        hasIcon: true,
+        onConfirm: () => {
+          this.data.splice(this.data.indexOf(data), 1)
+          this.$buefy.toast.open('Item deletado!')
+        },
+      })
+    },
+    ativarAlterar(data) {
+      this.data.forEach((inf) => {
+        if (inf.podeAlterar) {
+          inf.podeAlterar = false
+        }
+      })
+      this.alterarNome = data.nome_produto
+      this.alterarPreco = data.preco_produto
+      this.alterarQuantidade = data.quantidade
+      data.podeAlterar = true
+    },
+    cancel(data) {
+      data.podeAlterar = false
+    },
+    alterar(data) {
+      const index = this.data.indexOf(data)
+      data.nome_produto = this.alterarNome
+      data.preco_produto = this.alterarPreco
+      data.quantidade = this.alterarQuantidade
+      this.data[index] = data
+      data.podeAlterar = false
     },
   },
 }
 </script>
+<style scoped>
+.icon-atualizar {
+  display: flex;
+}
+</style>
