1168463ceb6fe7dcd1775e38da0d3204442b9c9e
Ysmael Braga <ysmaelbraga.prestserv@petrobras.com.br>
2022-07-23 00:47:16 -0300

    informacao extra da tela inicial

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <section></section>
+  <section>
+    <h1 class="manutencao">
+      <strong
+        >Em manutenção...
+
+        <p class="mt_40">
+          <nuxt-link to="/lista-compras">
+            <b-button type="is-primary">Lista de compras</b-button></nuxt-link
+          >
+        </p></strong
+      >
+    </h1>
+  </section>
 </template>
 
 <script>
@@ -8,4 +20,9 @@ export default {
   components: {},
 }
 </script>
-<style lang="scss"></style>
+<style lang="scss">
+.manutencao {
+  text-align: center;
+  font-size: 5rem;
+}
+</style>
