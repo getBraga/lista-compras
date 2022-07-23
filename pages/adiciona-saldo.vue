2fd3b2c8527fd78e864c36a5a18a6ae672b767a1
Ysmael Braga <ysmaelbraga.prestserv@petrobras.com.br>
2022-07-23 00:47:38 -0300

    inicio da tela de adicionar-saldo

--- pages/adiciona-saldo.vue
+++ pages/adiciona-saldo.vue
@@ -1,13 +1,29 @@
 <template>
-  <div>Inserir valor para compras</div>
+  <section>
+    <div class="h1_titulo">
+      <h1>
+        <strong>Adicionar Saldo</strong>
+      </h1>
+    </div>
+    {{ lista }}
+    <b-button type="is-primary" class="mb_20 mt_10">Novo item</b-button>
+  </section>
 </template>
 
 <script>
 export default {
-name: "TestePage"
+  computed: {
+    lista() {
+      return this.$store.state.lista
+    },
+  },
+  // mounted() {
+  //   this.$store.commit(
+  //     'GET_DADOS',
+  //     JSON.parse(window.localStorage.getItem('listaCompras'))
+  //   )
+  // },
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
