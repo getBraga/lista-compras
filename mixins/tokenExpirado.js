73bcbab440ad39b6afb337736c3c7dd42dbae463
Ysmael Braga <ysmaelbraga01@gmail.com>
2022-07-31 21:46:23 -0300

    mensagem de erro caso o token estiver expirado

---
+++ mixins/tokenExpirado.js
@@ -0,0 +1,8 @@
+export default function tokenExpirado(request) {
+  if (request === 'Auth token is expired') {
+    request = 'O token do usuário atual expirou.'
+    window.localStorage.accessToken = ''
+    window.localStorage.uid = ''
+  }
+  return request
+}
