f73efc3a02e0b139987d1af7de707dc179655abc
Ysmael Braga <ysmaelbraga01@gmail.com>
2022-08-01 00:27:47 -0300

    usandoo store para salvar os dados do saldo

--- store/index.js
+++ store/index.js
@@ -3,12 +3,19 @@ import services from '~/services/services'
 export const state = () => ({
   login: false,
   usuario: [],
-  lista: [],
+  saldo: 0,
 })
 export const mutations = {
   GET_USUARIO(state, payload) {
     state.login = true
     state.usuario = payload
+  },
+  GET_SALDO(state, payload) {
+    let soma = 0
+    for (const index in payload) {
+      soma += +payload[index].valor_saldo
+    }
+    state.saldo = soma
   },
 }
 
@@ -16,9 +23,11 @@ export const actions = {
   async getUsuario({ commit }) {
     const { data } = await services.getUser()
 
-    const [usuario] = Object.keys(data).map((i) => data[i])
-    const primeiroNome = usuario.nome.replace(/ .*/, '')
-    usuario.primeiroNome = primeiroNome
-    commit('GET_USUARIO', usuario)
+    if (data) {
+      const [usuario] = Object.keys(data).map((i) => data[i])
+      const primeiroNome = usuario.nome.replace(/ .*/, '')
+      usuario.primeiroNome = primeiroNome
+      commit('GET_USUARIO', usuario)
+    }
   },
 }
